a93e7e85b076fc612c2f84160c27c76a08b4bb0f
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2022-12-15 12:39:33 +0530

    ADD : course component

---
+++ courses/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Course(models.Model):
+    course_no = models.CharField(max_length=4)
+    course_name = models.CharField(max_length=200)
+    course_weight = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
